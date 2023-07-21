d852665f10d72ec860a61480ceff7ab83c0d4ff0
Guggenberger <github2022@guggenberger.me>
2023-07-21 19:14:19 +0200

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,30 @@
 #include <Arduino.h>
 #include <SPI.h>
+#include "PCF8574.h"
 #include <ESP8266WiFi.h> 
-#include <PubSubClient.h>   
-#include <ArduinoOTA.h>
+#include <PubSubClient.h>  
+#include <OneWire.h>
 
-const byte relais_A = D7;       
+/////////////////////////////////////////////////////////////////////////// PCF8574 Adresse
+PCF8574 pcf8574(0x20);
+//PCF8574 pcf8574_2(0x21);
+/*
+PINOUT Wemos Expander
+D1 SCL
+D2 SDA
+Pinout -> https://bre.is/9H5nddhG
+*/
+
+/////////////////////////////////////////////////////////////////////////// Schleifen verwalten
+unsigned long previousMillis_mqttCHECK = 0; // mqtt Abfragen
+unsigned long interval_mqttCHECK = 500; 
 
 
-// Kartendaten
+/////////////////////////////////////////////////////////////////////////// Variablen
+int mqtt_variablen_einmalig_anlegen = 0;
+
+
+/////////////////////////////////////////////////////////////////////////// Kartendaten
 
 const char* kartenID = "Relaiskarte_!";
 
@@ -19,38 +36,56 @@ const char* ssid = "ANKE581";
 const char* password = "q8d67nH5mH";
 const char* mqtt_server = "192.168.1.3";
 
+/////////////////////////////////////////////////////////////////////////// Funktionsprototypen
+void loop                       ();
+void callback                   (char* topic, byte* payload, unsigned int length);
+void reconnect                  ();
+void setuo                      ();
+void mqtt_connected             ();
+
 
 void callback(char* topic, byte* payload, unsigned int length) {
 
+/*
 Serial.println("Callback - load");
-
 payload[length] = '\0';
-
    Serial.print("payload: [");
    Serial.print((char *)payload);
    Serial.println("]");
+*/
 
-
-    if (strcmp(topic,"Relais1/status")==0) {
+    if (strcmp(topic,"ANKE581/RK001/IN/Relais/A")==0) {
 
         // Kanal A
         if ((char)payload[0] == 'o' && (char)payload[1] == 'n') {  
                  Serial.println("relais_A -> AN");
-                 digitalWrite(relais_A, HIGH);
-                 //client.publish("Relaiskarte/Weihnachten/24V_1","on");
-                delay(100);
+                 pcf8574.digitalWrite(P0, !HIGH);
               }
 
         if ((char)payload[0] == 'o' && (char)payload[1] == 'f' && (char)payload[2] == 'f') {  
                  Serial.println("relais_A -> AUS");
-                 digitalWrite(relais_A, LOW);
-                 //client.publish("Relaiskarte/Weihnachten/24V_1","off");
-                delay(100);
+                 pcf8574.digitalWrite(P0, !LOW);
+
               }
       } 
+
+
+    if (strcmp(topic,"ANKE581/RK001/IN/Relais/B")==0) {
+
+        // Kanal A
+        if ((char)payload[0] == 'o' && (char)payload[1] == 'n') {  
+                 Serial.println("relais_A -> AN");
+                 pcf8574.digitalWrite(P1, !HIGH);
+              }
+
+        if ((char)payload[0] == 'o' && (char)payload[1] == 'f' && (char)payload[2] == 'f') {  
+                 Serial.println("relais_A -> AUS");
+                 pcf8574.digitalWrite(P1, !LOW);
+              }
+      } 
+
+
 }
-
-
 
 void reconnect() {
   // Loop until we're reconnected
@@ -60,10 +95,13 @@ void reconnect() {
     if (client.connect(kartenID,"anke581","Sa,7+CGPfA9e6)y!")) {
       //Serial.println("connected");
       ////////////////////////////////////////////////////////////////////////// SUBSCRIBE Eintraege
-      client.subscribe("Relais1/status");
-      //client.publish("Relais1", "off");
-      client.publish("outTopic", "Online");
+        client.subscribe("ANKE581/RK001/IN/Relais/A");
+        client.publish("ANKE581/RK001/IN/Relais/A", "Online"); // mqtt Bekanntgabe
 
+        client.subscribe("ANKE581/RK001/IN/Relais/B");
+        client.publish("ANKE581/RK001/IN/Relais/B", "Online"); // mqtt Bekanntgabe
+      
+      
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -76,12 +114,28 @@ void reconnect() {
 
 void setup() {
 
-//Pins deklarieren
-  pinMode(relais_A, OUTPUT);
+/////////////////////////////////////////////////////////////////////////// Konfig Relais
+  pcf8574.pinMode(P0, OUTPUT);
+  pcf8574.pinMode(P1, OUTPUT);
+  pcf8574.pinMode(P2, OUTPUT);
+  pcf8574.pinMode(P3, OUTPUT);
+  pcf8574.pinMode(P4, OUTPUT);
+  pcf8574.pinMode(P5, OUTPUT);
+  pcf8574.pinMode(P6, OUTPUT);
+  pcf8574.pinMode(P7, OUTPUT);        
+  pcf8574.begin();
+/////////////////////////////////////////////////////////////////////////// 
 
-// Alle Relais auf LOW setzen
-
-  digitalWrite(relais_A, !LOW);
+/////////////////////////////////////////////////////////////////////////// Relais OFF
+pcf8574.digitalWrite(P0, !LOW);
+pcf8574.digitalWrite(P1, !LOW);
+pcf8574.digitalWrite(P2, !LOW);
+pcf8574.digitalWrite(P3, !LOW);
+pcf8574.digitalWrite(P4, !LOW);
+pcf8574.digitalWrite(P5, !LOW);
+pcf8574.digitalWrite(P6, !LOW);
+pcf8574.digitalWrite(P7, !LOW);
+/////////////////////////////////////////////////////////////////////////// 
 
 // MQTT Broker
   client.setServer(mqtt_server, 1883);
@@ -109,34 +163,44 @@ void setup() {
   Serial.println("Wifi connected...");
 
 
-// Start mit Relais klack bestaetigen
+}
 
-digitalWrite(relais_A, LOW);
-delay(300);
-digitalWrite(relais_A, HIGH);
-delay(300);
-digitalWrite(relais_A, LOW);
-delay(300);
-digitalWrite(relais_A, HIGH);
-delay(300);
-digitalWrite(relais_A, LOW);
+/////////////////////////////////////////////////////////////////////////// mqtt connected
+void mqtt_connected(){
 
+     // mqtt Daten senden     
+  if (!client.connected()) {
+      reconnect();
+    }
+    client.loop(); 
 
+}
+
+/////////////////////////////////////////////////////////////////////////// mqtt einmalig anlegen
+void mqtt_einmalig_anlegen(){
+
+ if (mqtt_variablen_einmalig_anlegen == 0) {
+  
+  // mqtt Publish
+  client.publish("ANKE581/RK001/IN/Relais/A", "off");
+  Serial.print("mqtt gesendet ...");
+  mqtt_variablen_einmalig_anlegen = 1;
+ }
 
 }
 
 
 void loop() {
 
-    // OTA Handle starten
-  ArduinoOTA.handle(); 
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ mqtt Checken
+  if (millis() - previousMillis_mqttCHECK > interval_mqttCHECK) {
+      previousMillis_mqttCHECK = millis(); 
+      mqtt_connected();
+    }
 
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ mqtt Variablen für IO Broker einmalig anlegen
+ mqtt_einmalig_anlegen();
 
-delay(100);
   
 }
 
